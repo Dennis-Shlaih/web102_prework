62b62e9c7929491866b3b4df0134bccb2e21bd08
Dennis Shlaih <dennisshlaih@gmail.com>
2025-01-25 20:45:35 -0500

    feat: Display top 2 pledged games
    - Added code to sort games based on the amount pledged in descending order.
    - Used destructuring and the spread operator to extract the top 2 games.
    - Created and appended new DOM elements to display the names of the games.

--- index.js
+++ index.js
@@ -166,7 +166,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...rest] = sortedGames
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameText = document.createElement('p');
+firstGameText.innerHTML = `${firstGame.name}`;
+firstGameContainer.appendChild(firstGameText);
 
 // do the same for the runner up item
+const secondGameText = document.createElement('p');
+secondGameText.innerHTML = `${secondGame.name}`;
+secondGameContainer.appendChild(secondGameText);
