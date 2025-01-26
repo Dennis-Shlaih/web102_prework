c5ac6383afad7820562b2f0537e07c13d83adcd7
Dennis Shlaih <dennisshlaih@gmail.com>
2025-01-26 15:29:35 -0500

    style: Enhance styling and improve responsiveness
    - Updated overall body padding and font settings.
    - Enhanced header styling with full-width and box-shadow.
    - Improved stats container with flex-wrap and centered alignment.
    - Adjusted game card styling for better visual appeal.
    - Added responsive design with media queries for smaller screens.
    - Improved search bar and button styling for a more cohesive look.
    - Enhanced navigation bar styling with improved hover effects and fixed positioning.

--- index.js
+++ index.js
@@ -193,12 +193,3 @@ function searchGame() {
 // Add an event listener to the search button
 searchBtn.addEventListener('click', searchGame);
 
-// function to add hamburger menu
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector("nav ul");
-
-    hamburger.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-    });
-});
